5fd7991e65d5dfe0c095a01f94f05e4e46c93f94
Mircea-Pavel ANTON <contact@mirceanton.com>
2025-01-25 15:43:55 +0200

    update?

---
+++ containers/devcontainer/files/fish/config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    # =========================================================================
+    # Commands to run in interactive sessions can go here
+    # =========================================================================
+    starship init fish | source
+    atuin init fish --disable-up-arrow | source
+end
+
+mise activate fish --shims | source
